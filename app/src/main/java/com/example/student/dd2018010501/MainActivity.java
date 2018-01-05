1edde638a7a325f11d522e30b5fa18a3074b98a9
kcli2412 <kcli2412@gmail.com>
2018-01-05 11:07:28 +0800

    add EditView setItems

--- app/src/main/java/com/example/student/dd2018010501/MainActivity.java
+++ app/src/main/java/com/example/student/dd2018010501/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +46,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         
+        builder.show();
+    }
+
+    public void click2(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Title");
+
+        final TextView tv = findViewById(R.id.textView);
+        final EditText td = new EditText(MainActivity.this);
+        builder.setView(td);
+        td.setText(tv.getText().toString());
+
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //Toast.makeText(MainActivity.this, "按下了確定", Toast.LENGTH_SHORT).show();
+
+                tv.setText(td.getText().toString());
+            }
+        });
+
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "按下了取消", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.show();
+    }
+
+    public void click3(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("列表");
+
+        final TextView tv2 = findViewById(R.id.textView2);
+
+        final String[] fruits = new String[] {"鳳梨", "蘋果", "西瓜", "蓮霧"};
+        builder.setItems(fruits, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                tv2.setText(fruits[i]);
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.setCancelable(false);
         builder.show();
     }
 }
